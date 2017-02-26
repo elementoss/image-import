d3e5a51275cb490a1b4ed4c294038e95b343d5a1
Konrad Kamiński <konrad.kaminski4@gmail.com>
2017-02-26 16:02:01 +0100

    fix

--- imageimport.sh
+++ imageimport.sh
@@ -30,7 +30,7 @@ do
 done
 
 for file in $(ls ${FROM_DIR}); do
-    if [[ ${file: -4} == .jpg ]] || [[ ${file: -4} == .cr2 ]] || [[ ${file: -4} == .nef ]]; then
+    if [[ ${file: -4} == .jpg ]] || [[ ${file: -4} == .JPG ]]  || [[ ${file: -4} == .cr2 ]] || [[ ${file: -4} == .CR2 ]] || [[ ${file: -4} == .nef ]]; then
         path_file=${FROM_DIR}/${file};
         date=$(date -r "$path_file" +%Y_%m_%d)
         mkdir -p "$TO_DIR/$date"
